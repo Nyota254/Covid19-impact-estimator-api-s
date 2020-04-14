3a5d6179110f3dd5a9e508e3144463311f7051b7
Nyota254 <knyota66@gmail.com>
2020-04-14 14:04:22 +0300

    remove trailing slashes for urls

--- covid_19_api/urls.py
+++ covid_19_api/urls.py
@@ -7,8 +7,8 @@ from .views import (home,
 
 urlpatterns = [
     path('',home,name="home"),
-    path('api/v1/on-covid-19/logs/',LogList.as_view()),
+    path('api/v1/on-covid-19/logs',LogList.as_view()),
     path('api/v1/on-covid-19/',CovidData.as_view()),
-    path('api/v1/on-covid-19/json/',CovidDataJson.as_view()),
-    path('api/v1/on-covid-19/xml/',CovidDataXML.as_view()),
+    path('api/v1/on-covid-19/json',CovidDataJson.as_view()),
+    path('api/v1/on-covid-19/xml',CovidDataXML.as_view()),
 ]
