242edba584020a0000c8b573b44de15ea599cf2d
Nyota254 <knyota66@gmail.com>
2020-04-14 11:39:55 +0300

    add trailing slashes to json and xml end points

--- covid_19_api/urls.py
+++ covid_19_api/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('',home,name="home"),
     path('api/v1/on-covid-19/logs/',LogList.as_view()),
     path('api/v1/on-covid-19/',CovidData.as_view()),
-    path('api/v1/on-covid-19/json',CovidDataJson.as_view()),
-    path('api/v1/on-covid-19/xml',CovidDataXML.as_view()),
+    path('api/v1/on-covid-19/json/',CovidDataJson.as_view()),
+    path('api/v1/on-covid-19/xml/',CovidDataXML.as_view()),
 ]
